3cae05761992706003a21a358cd7ed25f6ccb56e
Nafie <warrior259@gmail.com>
2022-11-05 21:31:36 +0300

    feat: add project3

--- project1/app.js
+++ project1/app.js
@@ -248,3 +248,18 @@ function validateFormData () {
     }
   })
 })()
+
+const duck = {
+  hasBill: true
+};
+const beaver = {
+  hasTail: true
+};
+const otter = {
+  hasFur: true,
+  feet: 'webbed'
+};
+
+const platypus = Object.assign(duck, beaver, otter);
+console.log(platypus)
+console.log(duck)
